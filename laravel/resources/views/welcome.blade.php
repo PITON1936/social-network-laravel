b698d0a1240a17a43843a8cd35162dba5dfae708
Vladimir <piton1936@gmail.com>
2017-03-30 17:44:47 +0300

    signup screen

---
+++ laravel/resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.master')
+
+@section('title')
+    Welcome!!
+    @endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Sign Up</h3>
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-Mail</label>
+                    <input type="text" class="form-control" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="first_name">Your First Name</label>
+                    <input type="text" class="form-control" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Your Password</label>
+                    <input type="password" class="form-control" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        <div class="col-md-6">
+            <h3>Sign In</h3>
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-Mail</label>
+                    <input type="text" class="form-control" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="first_name">Your First Name</label>
+                    <input type="text" class="form-control" name="email" id="email">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+    @endsection
